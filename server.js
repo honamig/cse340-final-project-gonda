bc79357166d5b14fee2f2beed04192ad71a46da2
Honami Gonda <honamigonda38@gmail.com>
2026-07-14 12:01:45 -0600

    global error handler

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ import reviewRoute from "./routes/reviewRoute.js";
 import serviceRequestRoute from "./routes/serviceRequestRoute.js";
 import employeeRoute from "./routes/employeeRoute.js";
 import ownerRoute from "./routes/ownerRoute.js";
+import connectPgSimple from "connect-pg-simple";
 
 dotenv.config();
 
@@ -22,7 +23,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+const PgSession = connectPgSimple(session);
+
 app.use(session({
+  store: new PgSession({
+    pool: pool,
+    schemaName: "dealership",
+    tableName: "session",
+    createTableIfMissing: true
+  }),
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
@@ -58,6 +67,18 @@ app.use("/", employeeRoute);
 app.use("/", ownerRoute);
 
 const PORT = process.env.PORT || 3000;
+
+app.use((req, res) => {
+  res.status(404).render("pages/errors/404", { title: "Page Not Found" });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).render("pages/errors/500", {
+    title: "Server Error",
+    message: process.env.NODE_ENV === "production" ? "Something went wrong on our end." : err.message
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
